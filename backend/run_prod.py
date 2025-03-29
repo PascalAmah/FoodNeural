a5f0aeae2c3d7f3447dde4ff1a76fb09ec08c9ec
pascalamah <pascal.amah10@gmail.com>
2025-03-29 23:19:47 +0100

    [fix]: api issues

--- backend/run_prod.py
+++
@@ -1,9 +0,0 @@
-from app import create_app
-import os
-
-if __name__ == "__main__":
-    if not os.getenv('FLASK_ENV'):
-        os.environ['FLASK_ENV'] = 'production'
-    
-    app = create_app()
-    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
