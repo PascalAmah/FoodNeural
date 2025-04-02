090a28e77a7f7f0c11f9533fda97a29651aef654
pascalamah <pascal.amah10@gmail.com>
2025-04-02 02:52:00 +0100

    [fix]: cors issues

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,10 +1,29 @@
+# from flask import Flask
+# from flask_cors import CORS
+# from app.config import Config
+
+# def create_app():
+#     app = Flask(__name__)
+#     CORS(app)
+    
+#     # Load config
+#     app.config.from_object(Config)
+    
+#     # Register blueprints
+#     from app.routes.api import api_bp
+#     app.register_blueprint(api_bp, url_prefix='/api')
+    
+#     return app
+
 from flask import Flask
 from flask_cors import CORS
 from app.config import Config
 
 def create_app():
     app = Flask(__name__)
-    CORS(app)
+    
+    # More specific CORS configuration
+    CORS(app, resources={r"/api/*": {"origins": "https://food-neural.vercel.app"}}, supports_credentials=True)
     
     # Load config
     app.config.from_object(Config)
