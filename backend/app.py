de2de926ecb56b1253937f8e87b37b65c9511fd9
pascalamah <pascal.amah10@gmail.com>
2025-04-03 13:29:48 +0100

    [fix]: render deployment

--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,8 @@
-# import os
-# import uvicorn
-# from app.main import app
-
-# if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))
-#     uvicorn.run(app, host="0.0.0.0", port=port)
-
-
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
